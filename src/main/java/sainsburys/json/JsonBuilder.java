81cee5167fdb59ce6ebd5de1dd5e5d45835f6c30
Ryan Fannin <rf3g14@soton.ac.uk>
2018-08-29 22:26:13 +0100

    Test steps mostly created

---
+++ src/main/java/sainsburys/json/JsonBuilder.java
@@ -0,0 +1,10 @@
+package sainsburys.json;
+
+public class JsonBuilder {
+
+    public String getFullJsonData(String htmlToScrape){
+
+
+        return null;
+    }
+}
--- src/test/java/sainsburys/Stepdefs.java
+++ src/test/java/sainsburys/Stepdefs.java
@@ -4,6 +4,8 @@ import cucumber.api.Scenario;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import sainsburys.json.JsonBuilder;
+import sainsburys.json.jsonBuilder;
 import sainsburys.utils.Constants;
 import sainsburys.utils.WebPageReader;
 import cucumber.api.java.en.Then;
@@ -13,6 +15,7 @@ public class Stepdefs {
 
 private Scenario scenario;
 private String rawData;
+private String jsonResult;
 
 @Before
 public void beforeTest(Scenario scenario){
@@ -28,22 +31,28 @@ public void beforeTest(Scenario scenario){
     @When("^I load the page$")
     public void i_load_the_page()  {
         WebPageReader reader = new WebPageReader();
+        scenario.write("Loading sainsburys webpage");
         rawData = reader.getHtmlFromUrl(Constants.CONNECTION_URL);
         assertTrue(rawData != null);
     }
 
     @When("^scrape the data$")
     public void scrape_the_data() {
-        
+        JsonBuilder jsonBuilder = new JsonBuilder();
+        jsonResult = jsonBuilder.getFullJsonData(rawData);
+
+        assertTrue(jsonResult != null);
     }
 
     @When("^the data is validated$")
     public void the_data_is_validated(){
+        String expectedJsonResult = null;
 
+        assertEquals(expectedJsonResult, jsonResult);
     }   
 
     @Then("^the website has been scraped correctly$")
     public void the_website_has_been_scraped_correctly(){
-
+        scenario.write("All tests passed");
     }
 }
